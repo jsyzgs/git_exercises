cf584e6d5b11769679b521a4adb6018569014d74
jsyzgs <jsyzgs@gmail.com>
2021-06-17 10:26:33 +0800

    feat: resize image

---
+++ examples.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def resize_image(image_path,save_path):
+    image_cv2 = cv2.imread(image_path)
+    new_image = cv2.resize(image_cv2,(256,128))
+    cv2.imwrite('new_image.jpg',new_image)
+
+if __name__ == '__main__':
+    image_path = './img.jpg'
+    save_path = './path'
+    resize_image(image_path,save_path)
+    print('done')
