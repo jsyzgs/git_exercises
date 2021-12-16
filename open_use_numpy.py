8729e0847a49c54c12bc501677fff1864f523c5c
jsyzgs <jsyzgs@gmail.com>
2021-12-16 16:36:13 +0800

    feat: add Image tobytes

--- open_use_numpy.py
+++ open_use_numpy.py
@@ -9,11 +9,10 @@ import numpy as np
 import cv2
 from PIL import Image
 
-
 file_path = r'C:\Users\GS\Desktop\rmb_100_shu.jpg'
-data_np = np.fromfile(file_path,dtype=np.byte)
+data_np = np.fromfile(file_path, dtype=np.byte)
 img = cv2.imread(file_path)
-ret,buf = cv2.imencode(".jpg",img)
+ret, buf = cv2.imencode(".jpg", img)
 
 img_bin = Image.fromarray(np.uint8(buf)).tobytes()
 print('done')
